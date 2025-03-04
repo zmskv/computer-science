32230338dec4ed253471ecbdaf4948ab85b079ed
zmskv <zmskvxd@gmail.com>
2025-03-05 00:41:18 +0300

    done

---
+++ 125_valid_palindrom.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+func cleanString(input string) string {
+	var result strings.Builder
+
+	for _, r := range input {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			result.WriteRune(r)
+		}
+	}
+
+	return result.String()
+}
+
+func isPalindrome(s string) bool {
+
+	validString := cleanString(s)
+	validString = strings.ToLower(validString)
+
+	lt := 0
+	rt := len(validString) - 1
+
+	for lt <= rt {
+		if validString[lt] != validString[rt] {
+			return false
+		}
+		lt++
+		rt--
+	}
+
+	return true
+}
+
+// ассимптотическая сложность O(n), где n - количество символов в строке
+// сложность по памяти O(n), из-за создания новой очищенной строки
