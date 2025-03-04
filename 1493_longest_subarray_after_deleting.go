e1be05917762aae9ff6cbaf38ea571461dad3978
zmskv <zmskvxd@gmail.com>
2025-03-04 16:33:27 +0300

    done

--- 1493_longest_subarray_after_deleting.go
+++ 1493_longest_subarray_after_deleting.go
@@ -1,2 +1,27 @@
 package main
 
+func longestSubarray(nums []int) int {
+	zeroCnt := 0
+	mxLenght := 0
+	start := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			zeroCnt++
+		}
+
+		for zeroCnt > 1 {
+			if nums[start] == 0 {
+				zeroCnt--
+			}
+			start++
+		}
+
+		mxLenght = max(mxLenght, i-start)
+	}
+
+	return mxLenght
+}
+
+// ассимптотическая сложность О(n), где n - количество чисел
+// сложность по памяти O(n), где n - количество чисел
