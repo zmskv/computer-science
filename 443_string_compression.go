f4a02d9dd2a5b45b11eec639150fadef7f953b2d
zmskv <zmskvxd@gmail.com>
2025-03-04 19:44:04 +0300

    done

---
+++ 443_string_compression.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strconv"
+)
+
+func compress(chars []byte) int {
+	read_ptr := 0
+	w_ptr := 0
+
+	for read_ptr < len(chars) {
+		char := chars[read_ptr]
+		cnt := 0
+
+		for read_ptr < len(chars) && chars[read_ptr] == char {
+			read_ptr++
+			cnt++
+		}
+		chars[w_ptr] = char
+		w_ptr++
+		if cnt > 1 {
+			cntStr := strconv.Itoa(cnt)
+			for i := 0; i < len(cntStr); i++ {
+				chars[w_ptr] = cntStr[i]
+				w_ptr++
+			}
+		}
+	}
+	return w_ptr
+}
+
+
+// ассимптотическая сложность O(n), где n - длина массива chars
+// сложность по памяти О(n), где n - длина массива chars
