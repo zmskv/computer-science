5e2ca89d56e8b764c62981abd189245af9a489bc
zmskv <zmskvxd@gmail.com>
2025-03-08 18:49:31 +0300

    done

---
+++ 1446_consecutive_characters.go
@@ -0,0 +1,24 @@
+package main
+
+func maxPower(s string) int {
+	mxLength := -1000
+	prev := s[0]
+	cnt := 1
+
+	for i := 1; i < len(s); i++ {
+		if prev != s[i] {
+			mxLength = max(mxLength, cnt)
+			cnt = 0
+		}
+		prev = s[i]
+		cnt++
+	}
+
+	mxLength = max(mxLength, cnt)
+
+	return mxLength
+
+}
+
+// ассимптотическая сложность O(n), где n - длина строки
+// сложность по памяти O(1)
---
+++ 20_valid_parentheses.go
@@ -0,0 +1,29 @@
+package main
+
+func isValid(s string) bool {
+	m := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+	stack := make([]rune, 0)
+
+	for _, ch := range s {
+		if _, exist := m[ch]; !exist {
+			stack = append(stack, ch)
+		} else {
+			if len(stack) == 0 {
+				return false
+			} else if stack[len(stack)-1] == m[ch] {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
+
+// ассимптотическая сложность O(n), где n - длина строки s
+// сложность по памяти O(n), гдe n - длина стека
