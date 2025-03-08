5a92310fba52fa5f284e138bf6b57f64f029043c
zmskv <zmskvxd@gmail.com>
2025-03-08 18:13:12 +0300

    done

---
+++ 3_longest_substring_without_repeating_characters.go
@@ -0,0 +1,28 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	table := make(map[rune]int)
+	lt, rt := 0, 0
+	res := 0
+
+	if len(s) == 1 {
+		return 1
+	}
+
+	for rt+1 < len(s)+1 {
+		if _, ok := table[rune(s[rt])]; !ok {
+			table[rune(s[rt])]++
+			rt++
+		} else {
+			delete(table, rune(s[lt]))
+			lt++
+		}
+		res = max(len(table), res)
+	}
+
+	return res
+}
+
+
+// ассимптотическая сложность O(n), где n - длина строки
+// сложность по памяти O(n), где n - количество символов в строке
