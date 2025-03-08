0c827c50f3ccd720f071e55a2820ddc596b35574
zmskv <zmskvxd@gmail.com>
2025-03-08 21:20:03 +0300

    done

---
+++ 232_implement_queue_using_stacks.go
@@ -0,0 +1,47 @@
+package main
+
+type MyQueue struct {
+	stackPush []int
+	stackPop  []int
+}
+
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.stackPush = append(this.stackPush, x)
+}
+
+func (this *MyQueue) transfer() {
+	if len(this.stackPop) == 0 {
+		for len(this.stackPush) > 0 {
+			top := this.stackPush[len(this.stackPush)-1]
+			this.stackPush = this.stackPush[:len(this.stackPush)-1]
+			this.stackPop = append(this.stackPop, top)
+		}
+	}
+}
+
+func (this *MyQueue) Pop() int {
+	this.transfer()
+	if len(this.stackPop) == 0 {
+		return -1
+	}
+
+	top := this.stackPop[len(this.stackPop)-1]
+	this.stackPop = this.stackPop[:len(this.stackPop)-1]
+	return top
+}
+
+func (this *MyQueue) Peek() int {
+	this.transfer()
+	if len(this.stackPop) == 0 {
+		return -1
+	}
+	return this.stackPop[len(this.stackPop)-1]
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.stackPush) == 0 && len(this.stackPop) == 0
+}
---
+++ 71_simplify_path.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+)
+
+func simplifyPath(path string) string {
+	components := strings.Split(path, "/")
+	stack := make([]string, 0)
+
+	for _, c := range components {
+		if c == "." || c == "" {
+			continue
+		} else if c == ".." {
+			if len(stack) != 0 {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			stack = append(stack, c)
+		}
+	}
+
+	return "/" + strings.Join(stack, "/")
+}
+
+// ассимптотическая сложность O(n), где n - длина строки
+// сложность по памяти O(n), где n - длина стека
