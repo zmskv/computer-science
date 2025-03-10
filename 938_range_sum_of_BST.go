70f32a2e39d98dc30a0dbe2128907c9d6dc51491
zmskv <zmskvxd@gmail.com>
2025-03-10 23:58:39 +0300

    done

---
+++ 938_range_sum_of_BST.go
@@ -0,0 +1,38 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	sum := 0
+	for val := low; val <= high; val++ {
+		if finder(root, val) {
+			sum += val
+		}
+	}
+
+	return sum
+
+}
+
+func finder(root *TreeNode, target int) bool {
+	if root == nil {
+		return false
+	}
+
+	if target > root.Val {
+		return finder(root.Right, target)
+	} else if target < root.Val {
+		return finder(root.Left, target)
+	} else if target == root.Val {
+		return true
+	}
+	return false
+}
+
+
+// ассимптотическая сложность O(m * n), где m - количество чисел в диапазоне low - high, n - высота дерева
+// сложность по памяти O(n), где n - высота дерева, тут оценивается глубина рекурсии
