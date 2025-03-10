6b4a26328ebd2ee825e7dbbfa6a4a0307b83dc7c
zmskv <zmskvxd@gmail.com>
2025-03-10 23:05:20 +0300

    done

---
+++ 49_group_anagrams.go
@@ -0,0 +1,25 @@
+package main
+
+func groupAnagrams(strs []string) [][]string {
+	m := make(map[[26]rune][]string)
+
+	for _, word := range strs {
+		count := [26]rune{}
+		for _, ch := range word {
+			count[ch-'a']++
+		}
+		m[count] = append(m[count], word)
+	}
+
+	ans := make([][]string, 0)
+
+	for _, val := range m {
+		ans = append(ans, val)
+	}
+
+	return ans
+
+}
+
+// ассимптотическая сложность O(n * k), где n - количество строк, k - количество букв в строках
+// сложность по памяти O(n * k), где n - количество списков, k - количество слов
