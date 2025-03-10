f31c6da859a69324f14b6c6629066d1e209a824d
zmskv <zmskvxd@gmail.com>
2025-03-10 19:34:10 +0300

    done

---
+++ 387_first_unique_character_in_a_string.go
@@ -0,0 +1,24 @@
+package main
+
+func firstUniqChar(s string) int {
+	m := make(map[rune]int)
+
+	for _, ch := range s {
+		if _, exist := m[ch]; exist {
+			m[ch]++
+		} else {
+			m[ch] = 1
+		}
+	}
+
+	for ind, ch := range s {
+		if m[ch] == 1 {
+			return ind
+		}
+	}
+
+	return -1
+}
+
+// ассимптотическая сложность O(n), где n - длина строки 
+// сложность по памяти O(n), где n - количество элементов в мапе
