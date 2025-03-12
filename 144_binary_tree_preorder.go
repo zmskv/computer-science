9b8cd3ff27a5858a1f266a7e268f3b1d03ada49c
zmskv <zmskvxd@gmail.com>
2025-03-13 00:15:03 +0300

    done

---
+++ 144_binary_tree_preorder.go
@@ -0,0 +1,20 @@
+package main
+
+func preorderTraversal(root *TreeNode) []int {
+	result := make([]int, 0)
+	traversal(root, &result)
+	return result
+}
+
+func traversal(node *TreeNode, result *[]int) {
+	if node == nil {
+		return
+	}
+	*result = append(*result, node.Val)
+	traversal(node.Left, result)
+	traversal(node.Right, result)
+}
+
+
+// сложность O(n), где n - количество вершин в дереве
+// сложность по памяти O(h), где h - высота дерева
