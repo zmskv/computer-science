75a3b142ad93db8e2dfdd3b364a3fd9488e4c3b0
zmskv <zmskvxd@gmail.com>
2025-03-13 10:48:06 +0300

    done

---
+++ 199_binary_tree_right_side_view.go
@@ -0,0 +1,25 @@
+package main 
+
+
+func rightSideView(root *TreeNode) []int {
+    return levelorder(root, 0, []int{})
+}
+
+func levelorder(node *TreeNode, level int, result []int) []int{
+	if node == nil{
+		return result
+	}
+
+	if level == len(result){
+		result = append(result, 0)
+	}
+
+	result[level] = node.Val
+	result = levelorder(node.Left, level + 1, result)
+	result = levelorder(node.Right, level + 1, result)
+
+	return result
+}
+
+// сложность O(n), где n - количество вершин в дереве
+// сложность по памяти O(n), где n - количество вершин
