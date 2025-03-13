254e6fcfc1548eea4af928748f5cef7d5f0e23ac
zmskv <zmskvxd@gmail.com>
2025-03-13 10:29:00 +0300

    done

---
+++ 102_binary_tree_order_traversal.go
@@ -0,0 +1,22 @@
+package main
+
+func levelOrder(root *TreeNode) [][]int {
+	return preorder(root, 0, [][]int{})
+}
+
+func preorder(node *TreeNode, level int, result [][]int) [][]int {
+	if node == nil {
+		return result
+	}
+	if level == len(result) {
+		result = append(result, []int{})
+	}
+	result[level] = append(result[level], node.Val)
+	result = preorder(node.Left, level+1, result)
+	result = preorder(node.Right, level+1, result)
+	return result
+}
+
+
+// сложность O(n), где n - количество вершин
+// сложность по памяти O(n), где n - количество вершин
