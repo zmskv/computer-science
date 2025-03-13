24e31529178111bd32c15ff94b9b5a3dfd99967a
zmskv <zmskvxd@gmail.com>
2025-03-13 21:10:52 +0300

    done

---
+++ 53_maximum_subarray.go
@@ -0,0 +1,18 @@
+package main
+
+import "math"
+
+func maxSubArray(nums []int) int {
+	sum := 0
+	mxSum := math.MinInt
+
+	for _, val := range nums {
+		sum = max(sum+val, val)
+		mxSum = max(mxSum, sum)
+	}
+
+	return mxSum
+}
+
+// сложность O(n), где n - длина nums
+// сложность по памяти O(1)
