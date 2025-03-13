14e7075da79814c498e3bbe2aebe939b539940c4
zmskv <zmskvxd@gmail.com>
2025-03-13 11:32:22 +0300

    done

---
+++ 101_symmetric_tree.go
@@ -0,0 +1,25 @@
+package main
+
+func isSymmetricTree(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return check(root.Left, root.Right)
+}
+
+func check(left *TreeNode, right *TreeNode) bool {
+	if left == nil || right == nil {
+		return left == nil && right == nil
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return check(left.Left, right.Right) && check(left.Right, right.Left)
+}
+
+
+// сложность O(n), где n - высота дерева
+// сложность по памяти O(h), где h - высота дерева 
