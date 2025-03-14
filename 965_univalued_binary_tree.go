7e6e4f8e17796f02a46d651f743d1e590d1856b4
zmskv <zmskvxd@gmail.com>
2025-03-14 11:13:31 +0300

    done

---
+++ 965_univalued_binary_tree.go
@@ -0,0 +1,28 @@
+package main
+
+
+func isUnivalTree(root *TreeNode) bool {
+	return uniPreorder(root, map[int]int{})
+}
+
+func uniPreorder(node *TreeNode, m map[int]int) bool {
+	if node == nil {
+		return true
+	}
+
+	if _, exist := m[node.Val]; !exist {
+		m[node.Val] = 1
+	} else {
+		m[node.Val]++
+	}
+	if len(m) != 1 {
+		return false
+	}
+
+	return uniPreorder(node.Left, m) && uniPreorder(node.Right, m)
+
+}
+
+// сложность O(n), где n - количество вершин в дереве
+// сложность по памяти O(n), в худшем случае количество вершин в дереве а так можно сказать что высота
+
