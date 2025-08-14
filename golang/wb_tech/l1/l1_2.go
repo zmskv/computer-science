21bd7985a4f25614f48095c7a297dd91f018a43c
zmskv <zmskvxd@gmail.com>
2025-08-14 14:44:14 +0300

    l1_2 done

---
+++ golang/wb_tech/l1/l1_2.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Square(number int) int {
+	return number * number
+
+}
+func Example_L1_2() {
+	slice := []int{2, 4, 6, 8, 10}
+
+	var wg sync.WaitGroup
+
+	wg.Add(len(slice))
+	for _, number := range slice {
+		go func(number int) {
+			defer wg.Done()
+			fmt.Println(Square(number))
+		}(number)
+	}
+	wg.Wait()
+}
