9899477daf03610d3c1538999074809df4b42286
zmskv <zmskvxd@gmail.com>
2025-08-15 23:23:50 +0300

    l1_19 done

---
+++ golang/wb_tech/l1/l1_19.go
@@ -0,0 +1,19 @@
+package l1
+
+import (
+	"fmt"
+)
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func Example_L1_19() {
+	input := "главрыба"
+	output := Reverse(input)
+	fmt.Println(output)
+}
